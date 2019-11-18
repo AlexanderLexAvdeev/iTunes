77c62964ee907d8454e443f3e746922f1149a0d2
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2019-11-18 15:29:00 +0300

    feat(ITunesActivity, res): add SearchView

--- app/src/main/java/com/regula/itunes/avdeevav/ITunesActivity.kt
+++ app/src/main/java/com/regula/itunes/avdeevav/ITunesActivity.kt
@@ -3,8 +3,10 @@ package com.regula.itunes.avdeevav
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 import kotlinx.android.synthetic.main.activity_itunes.*
 
@@ -23,7 +25,9 @@ class ITunesActivity : AppCompatActivity() {
 
         menuInflater.inflate(R.menu.menu_itunes_activity, menu)
 
-        return true
+        initSearchView(menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -32,5 +36,28 @@ class ITunesActivity : AppCompatActivity() {
             R.id.actionSearchFilter -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+
+    private fun initSearchView(menu: Menu) {
+
+        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
+        val searchView: SearchView = searchItem.actionView as SearchView
+
+        searchView.queryHint = resources.getString(R.string.menuActionSearchInITunes)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                Toast.makeText(this@ITunesActivity, "search...", Toast.LENGTH_SHORT).show()
+
+                return true
+            }
+
+        })
     }
 }
