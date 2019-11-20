8bf35a164a0d020ea8dfdafa9df9a48116ec5af2
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2019-11-20 10:40:21 +0300

    feat(SearchService, HttpClient): add search service and http client

---
+++ app/src/main/java/com/regula/itunes/avdeevav/repository/client/HttpClient.kt
@@ -0,0 +1,19 @@
+package com.regula.itunes.avdeevav.repository.client
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object HttpClient {
+
+    private const val url = "https://itunes.apple.com"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    fun getSearchService(): SearchService {
+
+        return retrofit.create(SearchService::class.java)
+    }
+}
---
+++ app/src/main/java/com/regula/itunes/avdeevav/repository/client/SearchService.kt
@@ -0,0 +1,16 @@
+package com.regula.itunes.avdeevav.repository.client
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+import com.regula.itunes.avdeevav.repository.SearchResults
+
+interface SearchService {
+
+    @GET("/search")
+    fun getResult(
+            @Query("term") term: String,
+            @Query("media") media: String
+    ): Call<SearchResults>
+}
